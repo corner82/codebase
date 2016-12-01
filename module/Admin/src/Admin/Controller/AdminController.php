680e5878e8279732c36a519c4d5dddf339d9fc3b
corner82 <311corner82@gmail.com>
2016-12-01 15:03:07 +0300

    Admin assignrole action silindi
    Admin assignrole action silindi

--- module/Admin/src/Admin/Controller/AdminController.php
+++ module/Admin/src/Admin/Controller/AdminController.php
@@ -397,29 +397,6 @@
      }
      
      /**
-      * action to attach role and assignments on interface
-      * @return ViewModel
-      * @author Mustafa Zeynel Dağlı
-      * @since 01/08/2016
-      */
-     public function assignroleAction()
-     {
-        $langCode = $this->getServiceLocator()
-                         ->get('serviceTranslator');
-        $requestUriRegulated = $this->getServiceLocator()
-                                    ->get('serviceTranslatorUrlRegulator');
-        $publicKey = $this->getServiceLocator()
-                          ->get('servicePublicKeyReader'); 
-         
-        $view = new ViewModel(array(
-            'requestUriRegulated' => $requestUriRegulated,
-            'langCode'            => $langCode,
-            'publicKey'           => $publicKey,
-        ));
-        return $view;
-     }
-     
-     /**
       * action for operation types definitions
       * @return ViewModel
       * @author Mustafa Zeynel Dağlı
