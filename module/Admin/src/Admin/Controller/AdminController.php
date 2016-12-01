9312b499784d3d6753e88570a3deff6cf35a605b
corner82 <311corner82@gmail.com>
2016-12-01 16:17:25 +0300

    Admin unt action silindi
    Admin unt action silindi

--- module/Admin/src/Admin/Controller/AdminController.php
+++ module/Admin/src/Admin/Controller/AdminController.php
@@ -48,32 +48,7 @@
         ));
         return $view;
      }
-     
-     
-     /**
-      * system units definitions admin panel
-      * @return ViewModel
-      * @author Mustafa Zeynel Dağlı
-      * @since 04/05/2016
-      */
-     public function untAction()
-     {
-         $langCode = $this->getServiceLocator()
-                            ->get('serviceTranslator');
-        $requestUriRegulated = $this->getServiceLocator()
-                            ->get('serviceTranslatorUrlRegulator');
-        $publicKey = $this->getServiceLocator()
-                            ->get('servicePublicKeyReader'); 
-         
-        $view = new ViewModel(array(
-            'requestUriRegulated' => $requestUriRegulated,
-            'langCode' => $langCode,
-            'publicKey' => $publicKey,
-        ));
-        return $view;
-     }
-     
-     
+
      /**
       * resources action page for ACL operations
       * @return ViewModel
