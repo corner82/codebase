cfece1534f4e1f467ea130bce05039c292e34dcd
corner82 <311corner82@gmail.com>
2016-12-01 16:09:48 +0300

    Admin clusteremp action silindi
    Admin clusteremp action silindi

--- module/Admin/src/Admin/Controller/AdminController.php
+++ module/Admin/src/Admin/Controller/AdminController.php
@@ -489,28 +489,6 @@
         return $view;
      }
      
-     /**
-      * admin page for cluster employees
-      * @return ViewModel
-      * @author Mustafa Zeynel Dağlı
-      * @since 31/08/2016
-      */
-     public function clusterempAction()
-     {
-        $langCode = $this->getServiceLocator()
-                         ->get('serviceTranslator');
-        $requestUriRegulated = $this->getServiceLocator()
-                                    ->get('serviceTranslatorUrlRegulator');
-        $publicKey = $this->getServiceLocator()
-                          ->get('servicePublicKeyReader'); 
-         
-        $view = new ViewModel(array(
-            'requestUriRegulated' => $requestUriRegulated,
-            'langCode'            => $langCode,
-            'publicKey'           => $publicKey,
-        ));
-        return $view;
-     }
 
  }
 
